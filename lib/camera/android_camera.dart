b62b5b69e6670742c74e64ab37fe761bc0fe42e0
lineno2 <hosun1804@gmail.com>
2023-03-08 00:16:22 +0900

    [ADD] android camera timer toolbar

--- lib/camera/android_camera.dart
+++ lib/camera/android_camera.dart
@@ -8,92 +8,178 @@ class GetAndroidCamera extends StatefulWidget {
 class _GetAndroidCameraState extends State<GetAndroidCamera> {
   bool _isSettingsOn = false;
   bool _isFlashOn = false;
-  bool _isTimerOn = false;
   bool _isRatioOn = false;
   bool _isMotionPhotoOn = false;
+  int _timerDelay = 0;
+  double _overlayHeight = 0;
 
   @override
   void initState() {
     super.initState();
   }
 
+  void _onTimerDelaySelected(int value) {
+    setState(() {
+      _timerDelay = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black, toolbarHeight: 0,),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 0,
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Expanded(
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                color: Colors.black,
-                height: 80.0,
-                width: double.infinity,
+              child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  color: Colors.black,
+                  height: 80.0,
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.settings,
+                            color:
+                                _isSettingsOn ? Colors.yellow : Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            _isSettingsOn = !_isSettingsOn;
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(
+                            _isFlashOn ? Icons.flash_on : Icons.flash_off,
+                            color: _isFlashOn ? Colors.yellow : Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            _isFlashOn = !_isFlashOn;
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon( 
+                          _timerDelay == 0 ? Icons.schedule :
+                          _timerDelay == 3 ? Icons.timer_3 :
+                          _timerDelay == 10 ? Icons.timer_10 : Icons.one_x_mobiledata,
+                            color: _timerDelay != 0 ? Colors.yellow : Colors.white),
+                        // onPressed: () {
+                        //   setState(() {
+                        //     _isTimerOn = !_isTimerOn;
+                        //   });
+                        // },
+                        onPressed: () {
+                          setState(() {
+                            _overlayHeight = _overlayHeight == 0 ? 80 : 0;
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.aspect_ratio,
+                            color: _isRatioOn ? Colors.yellow : Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            _isRatioOn = !_isRatioOn;
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(
+                            _isMotionPhotoOn
+                                ? Icons.motion_photos_on
+                                : Icons.motion_photos_off,
+                            color: _isMotionPhotoOn
+                                ? Colors.yellow
+                                : Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            _isMotionPhotoOn = !_isMotionPhotoOn;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 100),
+                height: _overlayHeight,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    IconButton(
-                      icon: Icon(Icons.settings,
-                          color: _isSettingsOn ? Colors.yellow : Colors.white),
+                    TextButton(
                       onPressed: () {
                         setState(() {
-                          _isSettingsOn = !_isSettingsOn;
+                          _overlayHeight = 0;
+                          _timerDelay = 0;
                         });
                       },
+                      child: Text('0s',
+                          style: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.bold)),
                     ),
-                    IconButton(
-                      icon: Icon(_isFlashOn ? Icons.flash_on : Icons.flash_off,
-                          color: _isFlashOn ? Colors.yellow : Colors.white),
+                    TextButton(
                       onPressed: () {
                         setState(() {
-                          _isFlashOn = !_isFlashOn;
+                          _overlayHeight = 0;
+                          _timerDelay = 1;
                         });
                       },
+                      child: Text('1s',
+                          style: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.bold)),
                     ),
-                    IconButton(
-                      icon: Icon(Icons.schedule,
-                          color: _isTimerOn ? Colors.yellow : Colors.white),
+                    TextButton(
                       onPressed: () {
                         setState(() {
-                          _isTimerOn = !_isTimerOn;
+                          _overlayHeight = 0;
+                          _timerDelay = 3;
                         });
                       },
+                      child: Text('3s',
+                          style: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.bold)),
                     ),
-                    IconButton(
-                      icon: Icon(Icons.aspect_ratio,
-                          color: _isRatioOn ? Colors.yellow : Colors.white),
+                    TextButton(
                       onPressed: () {
                         setState(() {
-                          _isRatioOn = !_isRatioOn;
+                          _overlayHeight = 0;
+                          _timerDelay = 10;
                         });
                       },
-                    ),
-                    IconButton(
-                      icon: Icon(
-                          _isMotionPhotoOn
-                              ? Icons.motion_photos_on
-                              : Icons.motion_photos_off,
-                          color:
-                              _isMotionPhotoOn ? Colors.yellow : Colors.white),
-                      onPressed: () {
-                        setState(() {
-                          _isMotionPhotoOn = !_isMotionPhotoOn;
-                        });
-                      },
+                      child: Text('10s',
+                          style: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.bold)),
                     ),
                   ],
                 ),
               ),
-            ),
-          ),
+            ],
+          )),
+
           Container(
             width: 150.0,
             height: 50.0,
             margin: EdgeInsets.fromLTRB(0, 0, 0, 25.0),
             child: ScopingWidget(),
           ),
+
           // Camera preview will go here
           Align(
             alignment: Alignment.bottomCenter,
@@ -189,56 +275,6 @@ class _ScopingWidgetState extends State<ScopingWidget> {
         ));
   }
 }
-
-// class _ScopingWidgetState extends State<ScopingWidget> {
-//   List<bool> _buttonValues = [false, true, false];
-
-//   void _handleButtonPressed(int buttonIndex) {
-//     setState(() {
-//       for (int i = 0; i < _buttonValues.length; ++i) {
-//         _buttonValues[i] = i == buttonIndex ? true : false;
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         color: Colors.black.withOpacity(0.7),
-//         borderRadius: BorderRadius.circular(50.0),
-//       ),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           NumberButton(
-//             label: '0.5',
-//             isActive: _buttonValues[0],
-//             index: 0,
-//             onPressed: _handleButtonPressed,
-//           ),
-//           NumberButton(
-//             label: '1.0',
-//             isActive: _buttonValues[1],
-//             index: 1,
-//             onPressed: _handleButtonPressed,
-//           ),
-//           NumberButton(
-//             label: '2.0',
-//             isActive: _buttonValues[2],
-//             index: 2,
-//             onPressed: _handleButtonPressed,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class ScopingWidget extends StatefulWidget {
-//   @override
-//   _ScopingWidgetState createState() => _ScopingWidgetState();
-// }
 
 class NumberButton extends StatefulWidget {
   final String label;
