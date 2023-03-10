b1646132a50139a2512dacb863cb5648479a0387
JIEEEN <tjtpp009@korea.ac.kr>
2023-03-10 12:33:28 +0900

    [ADD] speechBubble & text input

---
+++ lib/message/getmessage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:smart_me/message/ios/iosmessage.dart' as ios;
+import 'package:smart_me/message/android/androidmessage.dart' as and;
+
+class getMessage extends StatefulWidget {
+  @override
+  _getIOSMessageScreen createState() => _getIOSMessageScreen();
+}
+
+class _getIOSMessageScreen extends State<getMessage> {
+  @override
+  Widget build(BuildContext context) {
+    return ios.IOSMessageScreen();
+  }
+}
+
+// class _getAndroidMessage extends StatefulWidget {
---
+++ lib/message/ios/iosmessage.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:math';
+import 'dart:ui';
+
+class IOSMessageScreen extends StatefulWidget {
+  @override
+  _IOSMessageScreen createState() => _IOSMessageScreen();
+}
+
+class _IOSMessageScreen extends State<IOSMessageScreen> {
+  //키보드 show, hide
+  final FocusNode focusNode = FocusNode();
+  final _messageInputController = TextEditingController();
+  String messageInput = '';
+  double bubbleSize_width = 200.0, bubbleSize_height = 100.0;
+
+  void dispose() {
+    _messageInputController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        backgroundColor: Color(0xffdfdee4),
+        //padding: ,
+        middle: Text('새로운 메시지'),
+      ),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: Text(''),
+        ),
+        CustomPaint(
+          size: Size(bubbleSize_width, bubbleSize_height),
+          painter: SpeechBubble(
+              bubbleColor: Color(0xff60ba61), messageText: messageInput),
+        ),
+        CupertinoTextField(
+          controller: _messageInputController,
+          style: TextStyle(fontSize: 30),
+          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+          placeholder: "메시지를 입력하세요",
+          keyboardAppearance: Brightness.light,
+          onSubmitted: (String str) {
+            setState(() => messageInput = str);
+          },
+        )
+      ]),
+    );
+  }
+}
+
+// shift option f로 괄호 정렬
+
+class SpeechBubble extends CustomPainter {
+  Color bubbleColor;
+  String messageText;
+
+  SpeechBubble({required this.bubbleColor, required this.messageText});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final bubbleSize = Size(size.width, size.height * 0.8);
+    final tailSize = Size(size.width * 0.1, size.height - bubbleSize.height);
+    final fillet = bubbleSize.width * 0.1;
+    final tailStartPoint = Point(size.width * 0.82, bubbleSize.height);
+
+    final bubblePath = Path()
+      ..moveTo(0, fillet)
+      ..lineTo(0, bubbleSize.height - fillet)
+      ..quadraticBezierTo(0, bubbleSize.height, fillet, bubbleSize.height)
+      ..lineTo(bubbleSize.width - fillet, bubbleSize.height)
+      ..quadraticBezierTo(bubbleSize.width, bubbleSize.height, bubbleSize.width,
+          bubbleSize.height - fillet)
+      ..lineTo(bubbleSize.width, fillet)
+      ..quadraticBezierTo(bubbleSize.width, 0, bubbleSize.width - fillet, 0)
+      ..lineTo(fillet, 0)
+      ..quadraticBezierTo(0, 0, 0, fillet);
+
+    final tailPath = Path()
+      ..moveTo(tailStartPoint.x, tailStartPoint.y)
+      ..cubicTo(
+        tailStartPoint.x + (tailSize.width * 0.2),
+        tailStartPoint.y,
+        tailStartPoint.x + (tailSize.width * 0.6),
+        tailStartPoint.y + (tailSize.height * 0.2),
+        tailStartPoint.x + tailSize.width / 2,
+        tailStartPoint.y + tailSize.height,
+      )
+      ..cubicTo(
+        (tailStartPoint.x + tailSize.width / 2) + (tailSize.width * 0.2),
+        tailStartPoint.y + tailSize.height,
+        tailStartPoint.x + tailSize.width,
+        tailStartPoint.y + (tailSize.height * 0.3),
+        tailStartPoint.x + tailSize.width,
+        tailStartPoint.y,
+      );
+
+    bubblePath.addPath(tailPath, Offset(0, 0));
+
+    final paintDraw = Paint()
+      ..color = bubbleColor
+      ..style = PaintingStyle.fill;
+
+    canvas.drawPath(bubblePath, paintDraw);
+    writeText(canvas, size, this.messageText);
+  }
+
+  //말풍선 안에 text를 적어줌
+  void writeText(Canvas canvas, Size size, String text) {
+    TextSpan sp = TextSpan(style: TextStyle(fontSize: 30), text: text);
+    TextPainter tp = TextPainter(text: sp, textDirection: TextDirection.ltr);
+
+    tp.layout();
+    double dx = size.width / 2 - tp.width / 2;
+    double dy = size.height / 2 - tp.height / 2;
+
+    Offset offset = Offset(dx, dy);
+    tp.paint(canvas, offset);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
