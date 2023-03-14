a9ef3cebcd61b6c42c5242c3ed217ebe3aa8ff05
JIEEEN <tjtpp009@korea.ac.kr>
2023-03-14 20:54:35 +0900

    [MOD] message align, speechBubble dynamic syze

--- lib/message/ios/iosmessage.dart
+++ lib/message/ios/iosmessage.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'dart:math';
 import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:smart_me/message/ios/speeachBubble.dart';
 
 class IOSMessageScreen extends StatefulWidget {
@@ -14,7 +14,13 @@ class _IOSMessageScreen extends State<IOSMessageScreen> {
   final FocusNode _focusNode = FocusNode();
   final _messageInputController = TextEditingController();
   String messageInput = '';
-  double bubbleSize_width = 100.0, bubbleSize_height = 40.0;
+
+  //******************************************************//
+  //입력된 messageInput의 길이가 길어지면 bubbleSize_width는 늘어나고//
+  //일정한 길이 이상이 돼 문장이 두줄로 바뀌면 bubbleSize_height*2//
+  //startpoint_x는 늘어난 width만큼 뺴줘야함//
+  //******************************************************//
+
   List<Widget> _bubbleList = [];
 
   void dispose() {
@@ -24,12 +30,36 @@ class _IOSMessageScreen extends State<IOSMessageScreen> {
   }
 
   Widget _buildSpeechBubble(String messageInput) {
+    double tempSize_width = 0,
+        tempSize_height = 50.0,
+        temppoint_x = MediaQuery.of(context).size.width - 25.0,
+        temppoint_y = 0.0;
+    String tempMessage = '';
+
+    tempMessage = sliceText(messageInput.length, messageInput);
+
+    for (int i = 0; i < messageInput.length; i++) {
+      if (i <= 7) {
+        tempSize_width += 25;
+      }
+      if (i != 0 && i % 7 == 0) {
+        tempSize_height += 70;
+      }
+    }
     return Column(
       children: <Widget>[
-        CustomPaint(
-          size: Size(bubbleSize_width, bubbleSize_height),
-          painter: SpeechBubble(
-              bubbleColor: Color(0xff60ba61), messageText: messageInput),
+        Row(
+          children: <Widget>[
+            CustomPaint(
+              size: Size(tempSize_width, tempSize_height),
+              painter: SpeechBubble(
+                bubbleColor: Color(0xff60ba61),
+                messageText: tempMessage,
+                startpoint_x: temppoint_x,
+                startpoint_y: temppoint_y,
+              ),
+            ),
+          ],
         ),
         Padding(
           padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
@@ -61,9 +91,11 @@ class _IOSMessageScreen extends State<IOSMessageScreen> {
             padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
           ),
           Expanded(
-            // child: SingleChildScrollView(
-            child: Column(children: _bubbleList),
-            // ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: _bubbleList,
+              ),
+            ),
           ),
           Row(
             children: <Widget>[
--- lib/message/ios/speeachBubble.dart
+++ lib/message/ios/speeachBubble.dart
@@ -3,58 +3,78 @@ import 'package:flutter/cupertino.dart';
 import 'dart:math';
 import 'dart:ui';
 
+String sliceText(int textLen, String msg) {
+  String ret = '';
+  int temp = 0;
+  if (textLen < 8) {
+    return msg;
+  } else {
+    while (textLen >= 8) {
+      ret += msg.substring(temp, temp + 7) + '\n';
+      temp += 7;
+      textLen -= 8;
+    }
+    ret += msg.substring(temp);
+  }
+
+  return ret;
+}
+
 class SpeechBubble extends CustomPainter {
   Color bubbleColor;
   String messageText;
-  double startpoint_x = 200.0;
-  double startpoint_y = 0.0;
+  double startpoint_x;
+  double startpoint_y;
 
-  SpeechBubble({required this.bubbleColor, required this.messageText});
+  SpeechBubble(
+      {required this.bubbleColor,
+      required this.messageText,
+      required this.startpoint_x,
+      required this.startpoint_y});
 
   @override
   void paint(Canvas canvas, Size size) {
     final bubbleSize = Size(size.width, size.height * 0.8);
-    final tailSize = Size(size.width * 0.1, size.height - bubbleSize.height);
-    final fillet = bubbleSize.width * 0.11;
-    final tailStartPoint = Point(size.width * 0.82, bubbleSize.height);
+    final tailSize = Size(15, 8);
+    final fillet = bubbleSize.width * 0.09;
+    final tailStartPoint =
+        Point(startpoint_x - bubbleSize.width * 0.2, bubbleSize.height);
 
     final bubblePath = Path()
       ..moveTo(startpoint_x, startpoint_y + fillet)
       ..lineTo(startpoint_x, startpoint_y + bubbleSize.height - fillet)
       ..quadraticBezierTo(startpoint_x, startpoint_y + bubbleSize.height,
-          startpoint_x + fillet, startpoint_y + bubbleSize.height)
-      ..lineTo(startpoint_x + bubbleSize.width - fillet,
+          startpoint_x - fillet, startpoint_y + bubbleSize.height)
+      ..lineTo(startpoint_x - bubbleSize.width + fillet,
           startpoint_y + bubbleSize.height)
       ..quadraticBezierTo(
-          startpoint_x + bubbleSize.width,
+          startpoint_x - bubbleSize.width,
           startpoint_y + bubbleSize.height,
-          startpoint_x + bubbleSize.width,
+          startpoint_x - bubbleSize.width,
           startpoint_y + bubbleSize.height - fillet)
-      ..lineTo(startpoint_x + bubbleSize.width, startpoint_y + fillet)
-      ..quadraticBezierTo(startpoint_x + bubbleSize.width, startpoint_y,
-          startpoint_x + bubbleSize.width - fillet, startpoint_y)
-      ..lineTo(startpoint_x + fillet, startpoint_y)
+      ..lineTo(startpoint_x - bubbleSize.width, startpoint_y + fillet)
+      ..quadraticBezierTo(startpoint_x - bubbleSize.width, startpoint_y,
+          startpoint_x - bubbleSize.width + fillet, startpoint_y)
+      ..lineTo(startpoint_x - fillet, startpoint_y)
       ..quadraticBezierTo(
           startpoint_x, startpoint_y, startpoint_x, startpoint_y + fillet);
 
     final tailPath = Path()
-      ..moveTo(startpoint_x + tailStartPoint.x, startpoint_y + tailStartPoint.y)
+      ..moveTo(tailStartPoint.x, startpoint_y + tailStartPoint.y)
       ..cubicTo(
-        startpoint_x + tailStartPoint.x + (tailSize.width * 0.2),
+        tailStartPoint.x + (tailSize.width * 0.2),
         startpoint_y + tailStartPoint.y,
-        startpoint_x + tailStartPoint.x + (tailSize.width * 0.6),
+        tailStartPoint.x + (tailSize.width * 0.6),
         startpoint_y + tailStartPoint.y + (tailSize.height * 0.2),
-        startpoint_x + tailStartPoint.x + tailSize.width / 2,
+        tailStartPoint.x + tailSize.width / 2,
         startpoint_y + tailStartPoint.y + tailSize.height,
       )
       ..cubicTo(
-        startpoint_x +
-            (tailStartPoint.x + tailSize.width / 2) +
-            (tailSize.width * 0.2),
+        (tailStartPoint.x + tailSize.width / 2) + (tailSize.width * 0.2),
         startpoint_y + tailStartPoint.y + tailSize.height,
-        startpoint_x + tailStartPoint.x + tailSize.width,
+        tailStartPoint.x + tailSize.width,
         startpoint_y + tailStartPoint.y + (tailSize.height * 0.3),
-        startpoint_x + tailStartPoint.x + tailSize.width,
+        tailStartPoint.x + tailSize.width,
         startpoint_y + tailStartPoint.y,
       );
 
@@ -74,8 +94,8 @@ class SpeechBubble extends CustomPainter {
     TextPainter tp = TextPainter(text: sp, textDirection: TextDirection.ltr);
 
     tp.layout();
-    double dx = startpoint_x + size.width / 2 - tp.width / 2;
-    double dy = startpoint_y + size.height / 2 - tp.height / 2;
+    double dx = startpoint_x - size.width + size.width / 2 - tp.width / 2;
+    double dy = startpoint_y + size.height / 2 - tp.height / 1.5;
 
     Offset offset = Offset(dx, dy);
     tp.paint(canvas, offset);
